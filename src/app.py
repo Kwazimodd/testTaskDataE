86523482919090634743fb661f4a8e954ca29b1c
Linad2997 <mrcross622@gmail.com>
2021-05-09 21:31:23 +0300

    Last version? no time

--- src/app.py
+++ src/app.py
@@ -5,70 +5,87 @@ from pattern import p_app, p_movie, p_song
 
 
 class App:
-    def __init__(self, db, bucket):
-        self.database = db
+    def __init__(self, bucket):
         self.bucket = bucket
-        self.read = {}
 
     def run(self):
         while 1:
-            k = self.get_data(self.get_json())
-            self.update_database(k)
+            self.get_data(self.get_bucket())
 
-    def get_json(self):
+    def get_bucket(self):
         return requests.get(self.bucket + "files_list.data").content.splitlines()
 
     def get_data(self, arr):
-        args = []
+        conn = pymysql.connect(host="www.db4free.net", user="linad2997", password="london1997", database="cual2000")
+        cursor = conn.cursor()
         for i in arr:
-            k = requests.get(self.bucket + str(i)[2:-1:1]).json()
+            name = (str(i))[2:-1:1]
+            cursor.execute("""SELECT * FROM ID_used
+                            WHERE Id_used='{}';""".format(name))
+            a = cursor.fetchall()
+            print(a)
+            if len(a) > 0:
+                return
+            else:
+                cursor.execute("""INSERT INTO ID_used (Id_used) 
+                                  VALUES ("{}");""".format(name))
+                conn.commit()
+            k = requests.get(self.bucket + name).json()
             for j in k:
-                args.append(j)
-        return args
+                self.update_database(j)
 
-    def update_database(self, args):
+    def update_database(self, arg):
+        conn = pymysql.connect(host="www.db4free.net", user="linad2997", password="london1997", database="cual2000")
+        cursor = conn.cursor()
+        if arg["type"] == "app":
+            p = p_app
+            is_awesome = arg["data"]["genre"] == "Games"
+            cursor.execute(p.format(arg["data"]["name"],
+                                    arg["data"]["genre"],
+                                    arg["data"]["rating"],
+                                    arg["data"]["version"],
+                                    arg["data"]["size_bytes"],
+                                    int(is_awesome)))
+            conn.commit()
+            print(cursor.fetchall())
+        elif arg["type"] == "movie":
+            p = p_movie
 
-        cursor = 0  # get cursor from db
-        for arg in args:
-            if arg["type"] == "app":
-                p = p_app
-                """cursor.execute(p.format(arg["data"]["name"],
-                                        arg["data"]["genre"],
-                                        arg["data"]["rating"],
-                                        arg["data"]["version"],
-                                        arg["data"]["size_bytes"],
-                                        arg["data"]["is_awesome"]))
-                print(cursor.fetchall())"""
-            elif arg["type"] == "movie":
-                p = p_movie
+            original_title_normalized = ""
+            for i in arg["data"]["original_title"]:
+                if i.isalpha() or i.isdigit() or i.isspace():
+                    original_title_normalized += i.lower()
+            original_title_normalized = original_title_normalized.replace(" ", "_")
 
-                original_title_normalized = ""
-                for i in arg["data"]["original_title"]:
-                    if i.isalpha() or i.isdigit() or i.isspace():
-                        original_title_normalized += i.lower()
-                original_title_normalized = original_title_normalized.replace(" ", "_")
-                print(original_title_normalized)
+            print(p.format(arg["data"]["original_title"],
+                            arg["data"]["original_language"],
+                            arg["data"]["budget"],
+                                    int(arg["data"]["is_adult"]),
+                                    arg["data"]["release_date"],
+                                    original_title_normalized))
 
-                """cursor.execute(p.format(arg["data"]["original_title"],
-                                        arg["data"]["original_language"],
-                                        arg["data"]["budget"],
-                                        arg["data"]["is_adult"],
-                                        arg["data"]["release_date"],
-                                        original_title_normalized))
-                print(cursor.fetchall())"""
-            elif arg["type"] == "song":
-                p = p_song
-                now = datetime.datetime.now()
-                """cursor.execute(p.format(arg["data"]["artist_name"],
-                                        arg["data"]["title"],
-                                        arg["data"]["year"],
-                                        arg["data"]["release"],
-                                        now))
-                print(cursor.fetchall())"""
+            cursor.execute(p.format(arg["data"]["original_title"],
+                                    arg["data"]["original_language"],
+                                    arg["data"]["budget"],
+                                    int(arg["data"]["is_adult"]),
+                                    arg["data"]["release_date"],
+                                    original_title_normalized))
+            conn.commit()
+            print(cursor.fetchall())
+        elif arg["type"] == "song":
+            p = p_song
+            now = datetime.datetime.now()
+            cursor.execute(p.format(arg["data"]["artist_name"],
+                                    arg["data"]["title"],
+                                    arg["data"]["year"],
+                                    arg["data"]["release"],
+                                    now))
+            conn.commit()
+            print(cursor.fetchall())
 
     @staticmethod
     def main():
-        app = App("", "https://data-engineering-interns.macpaw.io/")
+        app = App("https://data-engineering-interns.macpaw.io/")
         app.run()
 
 
--- src/pattern.py
+++ src/pattern.py
@@ -1,9 +1,9 @@
-p_app = """INSERT INTO apps (name, genre, rating, version, size_bytes, is_awesome)
-           VALUES ({}, {}, {}, {}, {}, {});"""
+p_app = """INSERT INTO apps (name, genre, rating, version, size_bytes, is_awesome) 
+            VALUES ("{}", "{}", "{}", "{}", "{}", "{}");"""
 
 p_movie = """INSERT INTO movies 
-             (original_title, original_language, budget, is_adult, release_date, original_title_normalized)
-             VALUES ({}, {}, {}, {}, {}, {});"""
+             (original_title, original_language, budget, is_adult, release_date, original_title_normalized) 
+             VALUES ("{}", "{}", "{}", "{}", "{}", "{}");"""
 
-p_song = """INSERT INTO songs (artist_name, title, year, release, ingestion_time)
-             VALUES ({}, {}, {}, {}, {});"""
+p_song = """INSERT INTO songs (artist_name, title, year, release_, ingestion_time)
+             VALUES ("{}", "{}", "{}", "{}", "{}");"""
